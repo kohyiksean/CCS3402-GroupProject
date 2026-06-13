beb4a7d3694d642adbdcbacc10b8e321dafa58ee
Jnxyan <janiceng040803@gmail.com>
2026-06-13 16:01:25 +0800

    Initial commit

---
+++ src/main/java/GroupProject/RoomRentalManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package GroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomRentalManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomRentalManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GroupProject/RoomRentalManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package GroupProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomRentalManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
